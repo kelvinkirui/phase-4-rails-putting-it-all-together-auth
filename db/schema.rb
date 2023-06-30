b14a58a198552f95a1e5419b1d9ad4cb20d78dfb
kk <+>
2023-06-30 13:45:25 +0300

    created user and recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_30_095614) do
+ActiveRecord::Schema.define(version: 2023_06_30_100734) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "instructions"
+    t.integer "minutes_to_complete"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2023_06_30_095614) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
